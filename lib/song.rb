0ad89a10ec03534519c56f0afa5e5f0ed85310e0
Doug Fitzgerald <dfitzge1@conncoll.edu>
2018-10-01 16:47:00 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,7 @@
 class Song 
   attr_accessor :name, :artist: :genre
   @@count = 0 
+  @@genres = []
   
   def initialize
     @@count += 1 
@@ -10,6 +11,6 @@ class Song
     @@count 
   end 
   
-  
+  def self.genres 
    
 end 
